8cd9f0be2b91dddb46bec3171f4359bb368ac61e
NoahAldhous <naldhous@hotmail.co.uk>
2024-07-04 19:30:27 +0100

    home page styling wrapped up

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 import DecorativeLine from "@/components/DecorativeLine/DecorativeLine";
 
 export default function Home() {
-  return (
+  return <div className={styles.home}>
     <PageContainer>
       <main className={styles.main}>
         <section className={styles.contentContainer}>
@@ -39,5 +39,73 @@ export default function Home() {
         </section>
       </main>
     </PageContainer>
-  );
-}
+    <PageContainer>
+      <main className={styles.main}>
+        <section className={styles.contentContainer}>
+          <section className={styles.textContent}>
+            <Header text="noah aldhous"/>
+            <section className={styles.subHeaderContainer}>
+              <h2 className={styles.subHeader}>
+                full stack developer
+              </h2>
+            </section>
+            <section className={styles.introTextContainer}>
+              <DecorativeLine/>
+              <p className={styles.introText}>
+                intro text
+              </p>
+            </section>
+          </section>
+          <section className={styles.cardContent}>
+
+          </section>
+        </section>
+        <section className={styles.bottomButtonContainer}>
+          <section className={styles.currentTextBar}>
+            <DecorativeLine/>
+            <p className={styles.currentText}>
+              Noah is currently...
+            </p>
+          </section>
+          <section className={styles.contactButtonContainer}>
+            <button className={styles.contactButton}> contact me</button>
+          </section>
+        </section>
+      </main>
+    </PageContainer>
+    <PageContainer>
+      <main className={styles.main}>
+        <section className={styles.contentContainer}>
+          <section className={styles.textContent}>
+            <Header text="noah aldhous"/>
+            <section className={styles.subHeaderContainer}>
+              <h2 className={styles.subHeader}>
+                full stack developer
+              </h2>
+            </section>
+            <section className={styles.introTextContainer}>
+              <DecorativeLine/>
+              <p className={styles.introText}>
+                intro text
+              </p>
+            </section>
+          </section>
+          <section className={styles.cardContent}>
+
+          </section>
+        </section>
+        <section className={styles.bottomButtonContainer}>
+          <section className={styles.currentTextBar}>
+            <DecorativeLine/>
+            <p className={styles.currentText}>
+              Noah is currently...
+            </p>
+          </section>
+          <section className={styles.contactButtonContainer}>
+            <button className={styles.contactButton}> contact me</button>
+          </section>
+        </section>
+      </main>
+    </PageContainer>
+    </div>
+};
